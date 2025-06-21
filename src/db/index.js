ff42fbda5fef568095822e04316db17bca60b93a
harshit <harshit91735@gmail.com>
2025-06-21 23:18:52 +0530

    connected MongoDB

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB host : ${connectionInstance.connect.host}`);
+        
+    } catch (error) {
+        console.log("MONGODB connection error",error);
+        process.exit(1) // it is a process present in node.js which is running currently and process is written to take refrence and exit 
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,47 @@
+// whenever you tray to connect DB always use try-catch ot promises to handle errors
+//DB is always in another content means it takes time so always use async await 
+
+// require("dotenv").config({path:"./env"});
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+dotenv.config({path:"./env"});
+connectDB(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this is first approach to connect the database using the IIFEE standard
+/* 
+import express from "express";
+const app = express();
+
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // connecting the database 
+        app.on("error",(error)=>{ // adding listener sometime after connecting the database our express app may not able to connect to the database so we add the listener to handle the error
+            console.log("ERROR",error);
+            throw(error);   
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+    }
+    catch(error){
+        console.log("ERROR: ",error);
+        throw error;
+        
+    }
+})()
+*/
